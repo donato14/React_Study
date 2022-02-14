97f6cb0e75385626dd6f5c952f0e01e8a2478fab
Kang Kujun <donato14@naver.com>
2022-02-14 22:00:05 +0900

    🚧 Add React Hook Study

---
+++ hook-tutorial/src/App.js
@@ -0,0 +1,26 @@
+// import Counter from './Counter';
+// import { useState } from "react";
+// import Info from "./Info";
+import Average from "./Average";
+
+const App = () => {
+  // return <Counter />;
+  // return <Info />
+  return <Average />
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button onClick={() => {
+  //       setVisible(!visible);
+  //     }}
+  //     >
+  //       {visible ? '숨기기' : '보이기'}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+};
+
+
+export default App;
