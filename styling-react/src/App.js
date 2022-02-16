55e33175c911757f26cfaff987c19249e265cd38
Kang Kujun <donato14@naver.com>
2022-02-16 21:42:30 +0900

    🚧 Studying styled-component

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponents';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        {/* <SassComponent /> */}
+        <StyledComponent/>
       </div>
     );
   }
